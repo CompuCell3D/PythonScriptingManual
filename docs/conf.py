8e62670a770d987891ddb39c408d6f20eedee3d1
Maciek Swat <maciekswat@gmail.com>
2017-05-22 21:26:42 -0400

    changed style

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
+
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -85,6 +85,13 @@ todo_include_todos = False
 # html_theme = 'basic'
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
